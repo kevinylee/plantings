2135deb0cb55c8402b4c8aecad3ff835248f9df6
kevinylees <kevin@bridge.do>
2026-01-23 12:38:57 -0800

    input and submit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from 'react';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+    <div className="flex min-h-screen flex-col items-center flex-column justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text 3xl font-bold">Soil 🌱</h1>
+      <section>
+        <label>Amount (USD)</label>
+        <input 
+          className="border rounded"
+          type="number"
+          min="1"
+          step="0.01"
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
+      </section>      
+      <button className="btn" type="submit">Plant</button>
     </div>
   );
 }
