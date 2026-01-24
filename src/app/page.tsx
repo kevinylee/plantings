f3d34d06ea94d589cda4669cdafecab6776bd27a
kevinylees <kevin@bridge.do>
2026-01-23 16:45:00 -0800

    form element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center flex-column justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text 3xl font-bold">Soil 🌱</h1>
-      <section>
+      <form>
         <label>Amount (USD)</label>
         <input 
           className="border rounded"
@@ -14,7 +14,7 @@ export default function Home() {
           min="1"
           step="0.01"
         />
-      </section>      
+      </form>      
       <button className="btn" type="submit">Plant</button>
     </div>
   );
