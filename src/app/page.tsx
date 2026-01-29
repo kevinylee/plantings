f2b5e054f30df007bc4c322ed7f15d59c0d24763
kevinylees <kevin@bridge.do>
2026-01-28 20:30:20 -0800

    connected input to btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,44 @@
 
 import { useState } from 'react';
 
+
+
 export default function Home() {
+  // the live typing state
+  const [liveValue, setLiveValue] = useState("");
+  // Submitted state amount value
+  const [finalInputValue, setInputValue] = useState("");
+
+  // Handle changes in inpur field (to update the state)
+  const handleLiveChange = (event) => {
+    setLiveValue(event.target.value);
+  };
+
+  // Handle the button click (submits dollar amount)
+  const handleClick = (event) => {
+    event.preventDefault();
+    setInputValue(liveValue);
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center flex-column justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text 3xl font-bold">Soil 🌱</h1>
       <form>
         <label>Amount (USD)</label>
-        <input 
+        <input
+          required
+          value={liveValue}
+          onChange={handleLiveChange}
+          placeholder="Dollar Amount..."
+          id="donation"
           className="border rounded"
           type="number"
           min="1"
           step="0.01"
         />
-      </form>      
-      <button className="btn" type="submit">Plant</button>
+        <button onClick={handleClick} className="rounded bg-black text-white">Plant</button>
+      </form>
+      <p>Amount:{finalInputValue}</p>
     </div>
   );
 }
