1d284a18624e25402035b11d56d2e45f266a62e0
kevinylees <kevin@bridge.do>
2026-02-12 15:00:37 -0800

    routes and checkout form

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,49 @@
+import Stripe from "stripe";
+
+// accepts the Stripe key
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2024-06-20",
+});
+
+// creates an endpoint, POST /api/checkout
+export async function POST(req: Request) {
+    try {
+        const { amount } = await req.json(); // parse the amount requested, ex. { "amount": 25 }
+        
+        // validate the number: no negative values, no non-numbers, no manual requests
+        const num = Number(amount);
+        if (!Number.isFinite(num) || num <= 0) {
+            return Response.json({ error: "Invalid amount." }, { status: 400 });
+        }
+
+        // Stripe expects cents
+        const amountInCents = Math.round(num * 100);
+
+        
+        const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!; // will be used for the success and/or cancel page
+        // a Stripe checkout session
+        const session = await stripe.checkout.sessions.create({
+            mode: "payment", // one-time payment
+            payment_method_types: ["card"], // accept card payment
+            line_items: [
+                {
+                    quantity: 1,
+                    price_data: {
+                        currency: "usd",
+                        unit_amount: amountInCents,
+                        product_data: { name: "Planting Donation" },
+                    },
+                },
+            ], // what is being bought
+            success_url: `${baseUrl}/success`, // where to go after payment success
+            cancel_url: `${baseUrl}/cancel`, // where to go if payment is canceled
+        });
+
+        return Response.json({ url: session.url }); // return a session object
+    } catch (err: any) { // catch any errors if the payment fails
+        return Response.json(
+            { error: err?.message || "Servor error" },
+            { status: 500 }
+        );
+    }
+}
